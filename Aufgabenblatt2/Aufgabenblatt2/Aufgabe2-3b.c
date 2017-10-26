f5bb381e651c8517ef1db8fe2087124fa88c2340
PLA7IN <konstantin.wacker@hotmail.de>
2017-10-26 15:46:59 +0200

    strcat, include string.h, wait -> waitpid

--- Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-3b.c
+++ Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-3b.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 	int PID = getpid();
@@ -15,6 +16,6 @@ int main() {
 	}
 	sleep(1); //warten auf start von jedem prozess
 	int status;
-	wait(-1);
-	printf("... und tschüss.\n"); //Kind verabschiedet sich
+	waitpid(-1, &status, 0);
+	printf(strcat(getpid() == PID ? "I'm Parent" : "I'm child", "... und tschüss.\n")); //Kind verabschiedet sich
 }

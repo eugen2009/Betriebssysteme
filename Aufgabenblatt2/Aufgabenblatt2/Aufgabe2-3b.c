f4f59d75c7adeb2375606ecf7475fd2ddf43b536
PLA7IN <konstantin.wacker@hotmail.de>
2017-10-26 15:28:14 +0200

    3kid forking

--- Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-3b.c
+++ Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-3b.c
@@ -4,10 +4,15 @@
 #include <stdio.h>
 
 int main() {
+	int PID = getpid();
 	int i;
 	for (i = 0; i < 3; i++) { // 3maliges ausführen
-		if (fork() == 0) { //fork beginnt nach der "for"... ein Elternprozess mit 3 Kindern?
-			printf("Kind: PID: %d; PPID: %d\n", getpid(), getppid()); //Ausgabe der Kinder
+		if (getpid() == PID) {
+			if (fork() == 0) { //fork beginnt nach der "for"... ein Elternprozess mit 3 Kindern?
+				printf("Kind: PID: %d; PPID: %d\n", getpid(), getppid()); //Ausgabe der Kinder
+			} else {
+				printf("Hello, I'm a Parent with the PID: %d", getpid());
+			}
 		}
 	}
 	sleep(1); //warten auf start von jedem prozess

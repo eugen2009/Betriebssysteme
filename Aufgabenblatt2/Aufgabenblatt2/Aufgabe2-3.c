9db967b7ee3abcd8a892b11f21bae49d2aef536e
PLA7IN <konstantin.wacker@hotmail.de>
2017-10-26 14:33:08 +0200

    2-3

---
+++ Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-3.c
@@ -0,0 +1,20 @@
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+	int status = 10;
+	if (fork() == 0) {
+		printf("Hello, I'm a child. My PID is %d!\n", getpid());
+		printf("I'm the child and I'm done!\n");
+	}
+	else {
+		sleep(5);
+		printf("Waiting for my child to complete!\n");
+		wait(&status);
+		printf("Hello, I'm a parent. My PID is %d!\n", getpid());
+		printf("Status of my child: %d\n", WEXITSTATUS(status));
+		printf("I'm the parent and I'm done!\n");
+	}
+}

4f4b141087048b54af7612ea3508915b92d43d8a
Salpico <bengt.claas.rhodgess@stud.fh-luebeck.de>
2017-10-26 13:42:44 +0200

    Changes and final version

--- Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-1.c
+++ Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-1.c
@@ -4,9 +4,8 @@
 #include <stdio.h>
 
 int main() {
-	int status = 0;
-	int childid = fork(); 
-	if(childid == 0) {
+	int status = 10;
+	if(fork() == 0) {
 		sleep(2);
         	printf( "Hello, I'm a child. My PID is %d!\n", getpid() );
 		printf( "I'm the child and I'm done!\n");
@@ -14,8 +13,7 @@ int main() {
 		printf( "Waiting for my child to complete!\n");
 		wait(&status);
 		printf( "Hello, I'm a parent. My PID is %d!\n", getpid() );
-		printf( "Status of my child: %d + %d \n", WEXITSTATUS(status), status);
+		printf( "Status of my child: %d\n", WEXITSTATUS(status));
 		printf( "I'm the parent and I'm done!\n");
 	}
 }
-

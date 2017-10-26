495e7f05407a22af4e7f1e3b73a21e1db59366b3
PLA7IN <konstantin.wacker@hotmail.de>
2017-10-26 14:20:51 +0200

    Tab correction and sleep before wait

--- Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-2.c
+++ Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-2.c
@@ -5,15 +5,15 @@
 
 int main() {
 	int status = 10;
-        if(fork() == 0) {
-                printf( "Hello, I'm a child. My PID is %d!\n", getpid() );
-                printf( "I'm the child and I'm done!\n");
-        }else {
-                printf( "Waiting for my child to complete!\n");
-                wait( &status);
+	if(fork() == 0) {
+		printf( "Hello, I'm a child. My PID is %d!\n", getpid() );
+		printf( "I'm the child and I'm done!\n");
+	} else {
+		printf( "Waiting for my child to complete!\n");
 		sleep(2);
-                printf( "Hello, I'm a parent. My PID is %d!\n", getpid() );
-                printf( "Status of my child: %d\n", WEXITSTATUS(status));
-                printf( "I'm the parent and I'm done!\n");
-        }
+		wait( &status);
+		printf( "Hello, I'm a parent. My PID is %d!\n", getpid() );
+		printf( "Status of my child: %d\n", WEXITSTATUS(status));
+		printf( "I'm the parent and I'm done!\n");
+	}
 }

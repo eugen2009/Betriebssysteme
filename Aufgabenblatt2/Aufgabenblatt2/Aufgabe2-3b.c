a90d77177450cf4e987d1447ad46717c399ab194
PLA7IN <konstantin.wacker@hotmail.de>
2017-10-26 15:36:28 +0200

    wait(-1)

--- Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-3b.c
+++ Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-3b.c
@@ -5,18 +5,16 @@
 
 int main() {
 	int PID = getpid();
+	printf("Hello, I'm a Parent with the PID: %d\n", getpid());
 	int i;
 	for (i = 0; i < 3; i++) { // 3maliges ausführen
 		if (getpid() == PID) {
 			if (fork() == 0) { //fork beginnt nach der "for"... ein Elternprozess mit 3 Kindern?
 				printf("Kind: PID: %d; PPID: %d\n", getpid(), getppid()); //Ausgabe der Kinder
-			} else {
-				printf("Hello, I'm a Parent with the PID: %d\n", getpid());
-			}
 		}
 	}
 	sleep(1); //warten auf start von jedem prozess
 	int status;
-	wait(&status);
+	wait(-1);
 	printf("... und tschüss.\n"); //Kind verabschiedet sich
 }

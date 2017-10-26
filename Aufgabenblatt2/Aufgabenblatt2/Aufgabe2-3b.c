44ff1a8a26857fe29a8ce03308d2d87c3cdab755
PLA7IN <konstantin.wacker@hotmail.de>
2017-10-26 15:50:18 +0200

    } correction

--- Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-3b.c
+++ Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-3b.c
@@ -12,6 +12,7 @@ int main() {
 		if (getpid() == PID) {
 			if (fork() == 0) { //fork beginnt nach der "for"... ein Elternprozess mit 3 Kindern?
 				printf("Kind: PID: %d; PPID: %d\n", getpid(), getppid()); //Ausgabe der Kinder
+			}
 		}
 	}
 	sleep(1); //warten auf start von jedem prozess

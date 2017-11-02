b9337aa3d01a5d5c8533effeace953c8c20dc8f5
Salpico <bengt.claas.rhodgess@stud.fh-luebeck.de>
2017-11-02 09:07:55 +0100

    final version, all functions implemented

--- Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-5.c
+++ Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-5.c
@@ -7,42 +7,57 @@
 int main() {
 	int pid = getpid();
 	int pid2 = 0;
-	int kidcount;
+	int kidcount = 0;
+	int status1;
+	int status2;
+	int status3;
 	int i;
+	int j;
 	for (i = 0; i < 4; i++) {
 		if (getpid() == pid) {
 			fork();
-			printf("Hello, my PID is: %d, my parent's PID is %d\n", getpid(), getppid());
-			sleep(1);
+			if (getppid() == pid) {
+				printf("Hello, my PID is: %d, my parent's PID is %d\n", getpid(), getppid());
+				sleep(5);
+			}
 			kidcount++;
 		}
 	}
-	printf("%d\n", kidcount);
+	if (getpid() == pid){
+		sleep(1);
+		printf("My PID is %d and I'm waiting for %d of my children to complete.\n", getpid(), kidcount);
+		while((wait(&status1)) > 0);
+	}
 	kidcount = 0;
 	for (i = 0; i < 2; i++) {
-		if(getppid() == pid) {
+		if (getppid() == pid) {
 			fork();
-			printf("Hello, my PID is: %d, my parent's PID is %d\n", getpid(), getppid());
-			sleep(1);
+			if (getppid() != pid && getpid() != pid) {
+				printf("Hello, my PID is: %d, my parent's PID is %d\n", getpid(), getppid());
+				sleep(5);
+			}
 			kidcount++;
 		}
 	}
-	if (getppid() != pid) pid2 = getpid();
-	printf("%d\n", kidcount);
+	if (getppid() == pid){
+		sleep(1);
+		printf("My PID is %d and i'm waiting for %d of my children to complete.\n", getpid(), kidcount);
+		while((wait(&status2)) > 0);
+	} 
 	kidcount = 0;
+	if (getppid() != pid && getpid() != pid) pid2 = getpid();
 	if(getpid() == pid2) {
 		fork();
-	//	pid = getpid();
-		printf("Hello, my PID is: %d, my parent's PID is %d\n", getpid(), getppid());
-		sleep(1);
+		if(getppid() == pid2){
+			printf("Hello, my PID is: %d, my parent's PID is %d\n", getpid(), getppid());
+			sleep(5);
+		}
 		kidcount++;
 	}
-	if (getpid() == pid) {
-		int status;
-		if (getpid() == pid) printf("I'm the parent (with the PID: %d) and im waiting for one of my children to complete\n", getpid());
-		wait(&status);
+	if(getpid() == pid2){
+		sleep(1);
+		printf("My PID is %d and I'm waiting for %d of my children to complete.\n", getpid(), kidcount);
+		while((wait(&status3)) > 0);
 	}
-	char* parent = "I'm parent";
-	char* child = "I'm child";
-	printf("%s with the PID: %d and I'm done.\n", getpid() == pid ? parent : child, getpid());
+	printf("My PID is: %d and I'm done.\n", getpid());
 }

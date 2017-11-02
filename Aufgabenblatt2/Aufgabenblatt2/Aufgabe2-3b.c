c4e87d4ffbaae7232d53869f353320c8f52977de
Salpico <bengt.claas.rhodgess@stud.fh-luebeck.de>
2017-11-02 09:18:27 +0100

    final version, moved printf-function

--- Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-3b.c
+++ Aufgabenblatt2/Aufgabenblatt2/Aufgabe2-3b.c
@@ -5,22 +5,24 @@
 #include <string.h>
 
 int main() {
-	int PID = getpid();
-	printf("Hello, I'm a Parent with the PID: %d\n", getpid());
+	char* parent  = "I'm a parent";
+	char* child = "I'm a child";
+	int pid = getpid();
 	int i;
 	for (i = 0; i < 3; i++) {
-		if (getpid() == PID && fork() == 0) {
+		if (getpid() == pid && fork() == 0) {
 				printf("Child: PID: %d; PPID: %d\n", getpid(), getppid());
 		}
 	}
+	if (getpid() == pid) printf("Hello, I'm a Parent with the PID: %d\n", getpid());
 	sleep(1);
 	int status;
-	if (getpid() == PID) printf("I'm the parent (with the PID: %d) and im waiting for my child(ren) to complete\n", getpid());
+	if (getpid() == pid) printf("I'm the parent (with the PID: %d) and im waiting for one  my children to complete\n", getpid());
 	wait(&status);
-	if (getpid() == PID) {
-		printf("I'm parent");
-	} else {
-		printf("I'm child");
-	}
-	printf(" with the PID: %d and I'm done.\n", getpid());
+	//if (getpid() == pid) {
+	//	printf("I'm parent");
+	//} else {
+	//	printf("I'm child");
+	//}
+	printf("%s with the PID: %d and I'm done.\n", getpid() == pid ? parent : child, getpid());
 }

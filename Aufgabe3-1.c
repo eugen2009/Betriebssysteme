329d402dcc8581e07abed634f249ee4d2e1965cf
Salpico <bengt.claas.rhodgess@stud.fh-luebeck.de>
2017-11-10 13:44:41 +0100

    final version

--- Aufgabe3-1.c
+++ Aufgabe3-1.c
@@ -5,21 +5,21 @@
 #include <stdio.h>
 #include <string.h>
 
-void* multi() {
+int num = 4;
+void *multi(void *hello) {
+	sleep(1);
 	num = num * 2;
+	printf("Ausgabe Thread 1: %d\n", num);
 }
-void* add() {
+void *add(void *world) {
+//	sleep(1);
 	num = num + 2;
+	printf("Ausgabe Thread 2: %d\n",num);
 }
 int main() {
-	pthread_t threadid1;
-	pthread_t* structp = &threadid1;
-	int add() {
-		num = num + 2;
-	}
-	pthread_create(structp, NULL, multi, NULL);
-//	printf("Ausgabe: %d", num);
-	pthread_create(structp, NULL, add, NULL);
-	printf("Ausgabe: %d", num);
+	pthread_t p1, p2;
+	pthread_create(&p1, NULL, multi, NULL);
+	pthread_create(&p2, NULL, add, NULL);
+	pthread_join(p1, NULL);
+	pthread_join(p2, NULL);
 }
-

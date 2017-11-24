a6c969587ff7c615bf9fed263ef99f1bf7f4e3d2
Salpico <bengt.claas.rhodgess@stud.fh-luebeck.de>
2017-11-24 16:17:22 +0100

    3.4 newest

--- Aufgabenblatt3/Aufgabe3-4.c
+++ Aufgabenblatt3/Aufgabe3-4.c
@@ -7,8 +7,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
+#include <math.h>
 
-
+int min(int a, int b) {
+	if(a > b) return b;
+	else return a;
+}
 struct input {
 	int num;
 	int cT;
@@ -20,28 +24,36 @@ int curThread = 0;
 int timecount = 0;
 
 void *createThread(void *input) {
+	struct input *in = input;
+	int actual = in->cT;
 	printf("Hello from thread No. %d!\n", actual);
-	*Thread timmey[*input];
+	pthread_t arr[in->num];
 	int tcount = 0;
-	int maxThreads = input->num;
+	int maxThreads = in->num;
+	struct input threads[maxThreads];
 	pthread_t p1, p2;
-	sleep(in->num);
-	while(tcount < min(floor(maxThreads, divisor), 16)) {
+	int comp = min((int)floor(maxThreads/divisor), 16);
+	while(tcount < maxThreads) {
 		pthread_mutex_lock(&mutex);
 		curThread++;
 		struct input in;
-		in.num = maxThreads;
+		in.num = comp;
+		printf("Debug\n");
 		in.cT = curThread;
-		pthread_create(array[x], NULL, createThread, &in);
+		threads[tcount] = in;
+		pthread_create(&arr[tcount], NULL, createThread, &threads[tcount]);
 		pthread_mutex_unlock(&mutex);
 		tcount++;
  	}
-	pthread_join(p1, NULL);
+	int i;
+	for (i = 0; i < maxThreads; i++) pthread_join(arr[i], NULL);
         printf("Bye from thread No. %d!\n", actual);
 }
 
 int main() {
-	maxThreads = 4;
+	struct input maxThreads;
+	maxThreads.num = 26;
+	maxThreads.cT = curThread;
 	createThread(&maxThreads);
-	printf("Die Threads haben zusammen %d Sekunden gewartet.\n", timecount);
+	printf("Insgesamt erzeugte Threads: %d\n", curThread);
 }
